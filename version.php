5def53f84f1b5e39ff7852eb62c91c2bd5ebe824
Tharindu Dissanayake <tharindud@gmail.com>
2017-03-20 16:44:39 +0530

    Bumping version to 0.6
    Signed-off-by: Tharindu Dissanayake <tharindud@gmail.com>

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 // Copyright 2017, Tharindu Dissanayake <tharindud@gmail.com>.
 // Published under the MIT license.
 
-define("CONAN_WEB_VERSION", "0.5");
+define("CONAN_WEB_VERSION", "0.6");
 define("CONAN_WEB_URL", "https://github.com/tharindud/conan-web");
 ?>
